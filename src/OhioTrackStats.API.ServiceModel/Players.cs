aab1cc9e0332c01e24c7af36f1529a1e9e9a7268
Tony Morris <afmorris@outlook.com>
2016-04-13 23:04:35 -0400

    Initial creation of DTOs

--- src/OhioTrackStats.API.ServiceModel/Players.cs
+++ src/OhioTrackStats.API.ServiceModel/Players.cs
@@ -7,4 +7,22 @@ namespace OhioTrackStats.API.ServiceModel
     public class QueryPlayers : QueryBase<Player>
     {
     }
+
+    [Route("/players")]
+    public class UpdatePlayer
+    {
+        public Player Player { get; set; }
+    }
+
+    [Route("/players/{PlayerId}")]
+    public class DeletePlayer
+    {
+        public int PlayerId { get; set; }
+    }
+
+    [Route("/players")]
+    public class CreatePlayer
+    {
+        public Player Player { get; set; }
+    }
 }
